0fd826c50c29500ed58036b81a4eea09f885286d
bigkeehee <75559626+bigkeehee@users.noreply.github.com>
2020-12-09 20:23:58 +0700

    Create Exercise4_Phattarapol.R.py

---
+++ Exercise4_Phattarapol.R.py
@@ -0,0 +1,9 @@
+FE_SCORE = 25
+GB_SCORE = 35
+ICS_SCORE = 46
+CP = 85
+print("-"*3, "Your Score", "-"*3)
+print("Foundation English :", FE_SCORE)
+print("General Business :", GB_SCORE)
+print("Introduction to Computer Systems :", ICS_SCORE)
+print("Computer Programming :", CP)
