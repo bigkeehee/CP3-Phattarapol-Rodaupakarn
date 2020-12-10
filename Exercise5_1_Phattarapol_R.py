47afbb25da9296cf54821f3c7aec6c7fd266cab5
bigkeehee <75559626+bigkeehee@users.noreply.github.com>
2020-12-10 16:29:53 +0700

    d

---
+++ Exercise5_1_Phattarapol_R.py
@@ -0,0 +1,12 @@
+x = int(input())
+y = int(input())
+
+plus = x + y
+mins = x - y
+multiply = x * y
+divide = x / y
+
+print(x,'+',y,"=",plus)
+print(x,'-',y,"=",mins)
+print(x,'*',y,"=",multiply)
+print(x,'/',y,"=",divide)
