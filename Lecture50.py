af382d87e8a20cffef0331ff74ceae32186c4847
bigkeehee <75559626+bigkeehee@users.noreply.github.com>
2021-01-03 14:53:47 +0700

    Create Lecture50.py

---
+++ Lecture50.py
@@ -0,0 +1,13 @@
+def Plus(x,y):
+    print(x + y)
+def mins(x,y):
+    print(x - y)
+def multiply(x,y):
+    print(x * y)
+def divide(x,y):
+    print(x / y)
+divide(100,52)
+Plus(50,75)
+mins(50,25)
+multiply(5,2)
+
